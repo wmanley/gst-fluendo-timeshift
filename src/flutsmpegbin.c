56f3fc38c3b36dec1946c54aae4939550719a6b8
Krzysztof Konopko <krzysztof.konopko@youview.com>
2012-12-17 14:56:51 +0000

    Added initial implementation of the timeshifter bin.
    The idea is to instantiate tsparse and flumpegshifter within the bin,
    handle PMT message from tsparse and set pcr-pid property of the
    timeshifter.

---
+++ src/flutsmpegbin.c
@@ -0,0 +1,104 @@
+/* GStreamer Time Shifting Bin
+ * Copyright (C) 2012 YouView TV Ltd.
+ *
+ * Author: Krzysztof Konopko <krzysztof.konopko@youview.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "flutsmpegbin.h"
+
+GST_DEBUG_CATEGORY_EXTERN (ts_mpeg_bin);
+#define GST_CAT_DEFAULT ts_mpeg_bin
+
+G_DEFINE_TYPE (GstFluMPEGShifterBin, gst_flumpegshifter_bin, GST_TYPE_BIN);
+
+static void
+gst_flumpegshifter_bin_handle_message (GstBin * bin, GstMessage * msg);
+
+static void
+gst_flumpegshifter_bin_class_init (GstFluMPEGShifterBinClass * klass)
+{
+  //GObjectClass *gobject_class;
+  GstElementClass *gstelement_class;
+  GstBinClass *gstbin_class;
+
+  //gobject_class = G_OBJECT_CLASS (klass);
+  gstelement_class = GST_ELEMENT_CLASS (klass);
+  gstbin_class = GST_BIN_CLASS (klass);
+
+  gstbin_class->handle_message =
+      GST_DEBUG_FUNCPTR (gst_flumpegshifter_bin_handle_message);
+
+  gst_element_class_set_metadata (gstelement_class,
+      "Fluendo Time Shift for MPEG TS streams", "Generic/Bin",
+      "Provide time shift operations on MPEG TS streams",
+      "Krzysztof Konopko <krzysztof.konopko@youview.com>");
+}
+
+static void
+mirror_pad (GstElement * element, const gchar * static_pad_name, GstBin * bin)
+{
+  GstPad *orig_pad, *ghost_pad;
+
+  orig_pad = gst_element_get_static_pad (element, static_pad_name);
+  g_return_if_fail (orig_pad);
+
+  ghost_pad = gst_ghost_pad_new (static_pad_name, orig_pad);
+  gst_object_unref (orig_pad);
+  g_return_if_fail (ghost_pad);
+
+  g_return_if_fail (gst_element_add_pad (GST_ELEMENT (bin), ghost_pad));
+}
+
+static void
+gst_flumpegshifter_bin_init (GstFluMPEGShifterBin * ts_bin)
+{
+  GstBin *bin = GST_BIN (ts_bin);
+
+  ts_bin->tsparse = gst_element_factory_make ("mpegtsdemux", "tsparse");
+  ts_bin->timeshifter =
+      gst_element_factory_make ("flutimeshift", "flumpegshifter");
+
+  gst_bin_add_many (bin, ts_bin->tsparse, ts_bin->timeshifter, NULL);
+  g_return_if_fail (gst_element_link_many (ts_bin->tsparse, ts_bin->timeshifter,
+          NULL));
+
+  mirror_pad (ts_bin->tsparse, "sink", bin);
+  mirror_pad (ts_bin->timeshifter, "src", bin);
+}
+
+static void
+gst_flumpegshifter_bin_handle_message (GstBin * bin, GstMessage * msg)
+{
+  GstFluMPEGShifterBin *ts_bin = GST_FLUMPEGSHIFTER_BIN (bin);
+
+  if (gst_message_has_name (msg, "pmt")) {
+    guint pcr_pid;
+
+    const GstStructure *gs = gst_message_get_structure (msg);
+
+    if (!gst_structure_get_uint (gs, "pcr-pid", &pcr_pid)) {
+      GST_ERROR ("Cannot extract PCR PID");
+    }
+
+    GST_DEBUG ("Setting PCR PID: %u", pcr_pid);
+    g_object_set (ts_bin->timeshifter, "pcr-pid", pcr_pid, NULL);
+  }
+
+  GST_BIN_CLASS (gst_flumpegshifter_bin_parent_class)
+      ->handle_message (bin, msg);
+}
---
+++ src/flutsmpegbin.h
@@ -0,0 +1,61 @@
+/* GStreamer Time Shifting Bin
+ * Copyright (C) 2012 YouView TV Ltd.
+ *
+ * Author: Krzysztof Konopko <krzysztof.konopko@youview.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef __FLUTSMPEGBIN_H__
+#define __FLUTSMPEGBIN_H__
+
+#include <gst/gst.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_FLUMPEGSHIFTER_BIN \
+  (gst_flumpegshifter_bin_get_type())
+#define GST_FLUMPEGSHIFTER_BIN(obj) \
+  (G_TYPE_CHECK_INSTANCE_CAST((obj),GST_TYPE_FLUMPEGSHIFTER_BIN,GstFluMPEGShifterBin))
+#define GST_FLUMPEGSHIFTER_BIN_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_CAST((klass),GST_TYPE_FLUMPEGSHIFTER_BIN,GstFluMPEGShifterBinClass))
+#define GST_IS_FLUMPEGSHIFTER_BIN(obj) \
+  (G_TYPE_CHECK_INSTANCE_TYPE((obj),GST_TYPE_FLUMPEGSHIFTER_BIN))
+#define GST_IS_FLUMPEGSHIFTER_BIN_CLASS(klass) \
+  (G_TYPE_CHECK_CLASS_TYPE((klass),GST_TYPE_FLUMPEGSHIFTER_BIN))
+#define GST_FLUMPEGSHIFTER_BIN_CAST(obj) ((GstFluMPEGShifterBin *) (obj))
+
+typedef struct _GstFluMPEGShifterBin GstFluMPEGShifterBin;
+typedef struct _GstFluMPEGShifterBinClass GstFluMPEGShifterBinClass;
+
+struct _GstFluMPEGShifterBin
+{
+  GstBin parent_instance;
+
+  GstElement * tsparse;
+  GstElement * timeshifter;
+};
+
+struct _GstFluMPEGShifterBinClass
+{
+  GstBinClass parent_class;
+};
+
+GType gst_flumpegshifter_bin_get_type (void);
+
+G_END_DECLS
+
+#endif /* __FLUTSMPEGBIN_H__ */
